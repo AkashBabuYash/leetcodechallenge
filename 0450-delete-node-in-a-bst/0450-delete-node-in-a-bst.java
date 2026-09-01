48d556539a562bbbf1537d757300f29a78ff93f7
Akash Babu <babuakash43168@gmail.com>
2026-09-02 01:29:43 +0530

    Time: 0 ms (100%), Space: 47.7 MB (18.47%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -1,93 +1,24 @@
 class Solution {
-    public static TreeNode find(TreeNode root, int key) {
-        if (root == null) {
-            return null;
-        }
-
-        if (root.val == key) {
-            return root;
-        } 
-        else if (root.val < key) {
-            return find(root.right, key);
-        } 
-        else {
-            return find(root.left, key);
-        }
-    }
-
-    public static TreeNode successor(TreeNode temp) {
-        while (temp.left != null) {
-            temp = temp.left;
-        }
-        return temp;
-    }
-
     public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) {
-            return null;
+        if (root == null) return null;
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
         }
-
-        TreeNode temp = find(root, key);
-
-        if (temp == null) {
-            return root;
+        else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
         }
-
-        if (temp == root) {
-            if (temp.left == null && temp.right == null) {
-                return null;
-            }
-
-            if (temp.left == null) {
-                return temp.right;
-            }
-
-            if (temp.right == null) {
-                return temp.left;
-            }
-
-            TreeNode curr = successor(temp.right);
-            temp.val = curr.val;
-            temp.right = deleteNode(temp.right, curr.val);
-
-            return root;
-        }
-
-        TreeNode parent = root;
-
-        while (parent.left != temp && parent.right != temp) {
-            if (key < parent.val) {
-                parent = parent.left;
-            } else {
-                parent = parent.right;
-            }
-        }
-
-        if (temp.left == null && temp.right == null) {
-            if (parent.left == temp) {
-                parent.left = null;
-            } else {
-                parent.right = null;
-            }
-        } 
-        else if (temp.left == null) {
-            if (parent.left == temp) {
-                parent.left = temp.right;
-            } else {
-                parent.right = temp.right;
-            }
-        } 
-        else if (temp.right == null) {
-            if (parent.left == temp) {
-                parent.left = temp.left;
-            } else {
-                parent.right = temp.left;
-            }
-        } 
         else {
-            TreeNode curr = successor(temp.right);
-            temp.val = curr.val;
-            temp.right = deleteNode(temp.right, curr.val);
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+
+            TreeNode temp = root.right;
+            while (temp.left != null) {
+                temp = temp.left;
+            }
+
+            root.val = temp.val;
+            root.right = deleteNode(root.right, temp.val);
         }
 
         return root;
