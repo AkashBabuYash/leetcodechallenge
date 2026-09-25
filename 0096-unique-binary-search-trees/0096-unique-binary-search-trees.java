a154027f5140bb59f0c11e10d5c30e3fa2dcf9f5
Akash Babu <babuakash43168@gmail.com>
2026-09-26 00:10:23 +0530

    Time: 0 ms (100%), Space: 42.1 MB (30.84%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -3,9 +3,9 @@ class Solution {
         long ans = 1;
 
         for (int i = 1; i <= n; i++) {
-            ans = ans * 2 * (2 * i - 1) / (i + 1);
+            ans = ans * (n + i) / i;
         }
 
-        return (int) ans;
+        return (int)(ans / (n + 1));
     }
 }
