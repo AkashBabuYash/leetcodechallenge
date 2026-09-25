93077c5a1e5966ea50c136b50de77d4385d0bfb5
Akash Babu <babuakash43168@gmail.com>
2026-09-26 00:12:31 +0530

    Time: 1 ms (8.05%), Space: 42.2 MB (13.77%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,11 +1,28 @@
+import java.math.BigInteger;
+
 class Solution {
     public int numTrees(int n) {
-        long ans = 1;
+        BigInteger fact2n = BigInteger.ONE;
+        BigInteger factN = BigInteger.ONE;
+        BigInteger factN1 = BigInteger.ONE;
 
-        for (int i = 1; i <= n; i++) {
-            ans = ans * (n + i) / i;
+        // (2n)!
+        for (int i = 1; i <= 2 * n; i++) {
+            fact2n = fact2n.multiply(BigInteger.valueOf(i));
         }
 
-        return (int)(ans / (n + 1));
+        // n!
+        for (int i = 1; i <= n; i++) {
+            factN = factN.multiply(BigInteger.valueOf(i));
+        }
+
+        // (n + 1)!
+        for (int i = 1; i <= n + 1; i++) {
+            factN1 = factN1.multiply(BigInteger.valueOf(i));
+        }
+
+        BigInteger ans = fact2n.divide(factN1.multiply(factN));
+
+        return ans.intValue();
     }
 }
